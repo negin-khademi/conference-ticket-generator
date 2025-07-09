4f3de3ce983f45852cfc1dc418dc9dcc177f7992
negin-khademi <negin.khademi@gmail.com>
2025-07-10 02:05:58 +0330

    feat : create background component & use images as background

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { BackgroundComponent } from "./background/background.component";
+import { Component } from "@angular/core";
+import { RouterOutlet } from "@angular/router";
 
 @Component({
-  selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+	selector: "app-root",
+	imports: [RouterOutlet, BackgroundComponent],
+	templateUrl: "./app.component.html",
+	styleUrl: "./app.component.scss",
 })
 export class AppComponent {
-  title = 'conference-ticket-generator';
+	title = "conference-ticket-generator";
 }
