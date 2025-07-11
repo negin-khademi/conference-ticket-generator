d7e6094484c3912adbe9a5612debf7f0b7d18ba4
negin-khademi <negin.khademi@gmail.com>
2025-07-11 22:14:03 +0330

    feat : add some custom color in tailwind.config file

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,23 @@ module.exports = {
 			fontFamily: {
 				mono: ["Inconsolata", "monospace"],
 			},
+			fontSize: {
+				xxs: "0.6rem", // Customize the size as per your need
+			},
+		},
+		// Custom colors from style guide
+		colors: {
+			custom_neutral: {
+				0: "hsl(0, 0%, 100%)",
+				300: "hsl(252, 6%, 83%)",
+				500: "hsl(245, 15%, 58%)",
+				700: "hsl(245, 19%, 35%)",
+				900: "hsl(248, 70%, 10%)",
+			},
+			custom_orange: {
+				500: "hsl(7, 88%, 67%)",
+				700: "hsl(7, 71%, 60%)",
+			},
 		},
 	},
 	plugins: [],
