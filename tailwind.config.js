f68d05e3426acea90f4b2f676255e547ebedf16c
negin-khademi <negin.khademi@gmail.com>
2025-07-12 17:13:16 +0330

    feat : add background image and fontweight to theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,19 +9,27 @@ module.exports = {
 			fontSize: {
 				xxs: "0.6rem", // Customize the size as per your need
 			},
-		},
-		// Custom colors from style guide
-		colors: {
-			custom_neutral: {
-				0: "hsl(0, 0%, 100%)",
-				300: "hsl(252, 6%, 83%)",
-				500: "hsl(245, 15%, 58%)",
-				700: "hsl(245, 19%, 35%)",
-				900: "hsl(248, 70%, 10%)",
+			fontWeight: {
+				hairline: "50",
 			},
-			custom_orange: {
-				500: "hsl(7, 88%, 67%)",
-				700: "hsl(7, 71%, 60%)",
+			backgroundImage: (theme) => ({
+				"text-gradient": `linear-gradient(to right , ${theme(
+					"colors.custom_orange.700"
+				)},  #F5F5F5)`,
+			}),
+
+			colors: {
+				custom_neutral: {
+					0: "hsl(0, 0%, 100%)",
+					300: "hsl(252, 6%, 83%)",
+					500: "hsl(245, 15%, 58%)",
+					700: "hsl(245, 19%, 35%)",
+					900: "hsl(248, 70%, 10%)",
+				},
+				custom_orange: {
+					500: "hsl(7, 88%, 67%)",
+					700: "hsl(7, 71%, 60%)",
+				},
 			},
 		},
 	},
