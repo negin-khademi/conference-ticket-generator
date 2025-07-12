731a2fa9d2fdb41dbb2cec43709a46bcde182cbf
negin-khademi <negin.khademi@gmail.com>
2025-07-12 17:15:30 +0330

    feat : ui for ticket component completed

--- src/app/ticket/ticket.component.ts
+++ src/app/ticket/ticket.component.ts
@@ -1,11 +1,14 @@
-import { Component } from '@angular/core';
+import { DatePipe, TitleCasePipe } from "@angular/common";
+
+import { BackgroundComponent } from "../background/background.component";
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-ticket',
-  imports: [],
-  templateUrl: './ticket.component.html',
-  styleUrl: './ticket.component.scss'
+	selector: "app-ticket",
+	imports: [BackgroundComponent, TitleCasePipe, DatePipe],
+	templateUrl: "./ticket.component.html",
+	styleUrl: "./ticket.component.scss",
 })
 export class TicketComponent {
-
+	currentDateTime: Date = new Date();
 }
