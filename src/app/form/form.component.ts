714ac8e74d121ccecd63311cc8dac7b004a26316
negin-khademi <negin.khademi@gmail.com>
2025-07-14 19:10:49 +0330

    feat : send data from form to transfer service

--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -1,6 +1,7 @@
 import { Component, inject } from "@angular/core";
 
 import { BackgroundComponent } from "../background/background.component";
+import { DataTransferService } from "./../shared/services/data-transfer.service";
 import { FormsModule } from "@angular/forms";
 import { Router } from "@angular/router";
 import { TitleCasePipe } from "@angular/common";
@@ -13,7 +14,23 @@ import { TitleCasePipe } from "@angular/common";
 })
 export class FormComponent {
 	private router = inject(Router);
+
+	name!: string;
+	emailAdress!: string;
+	gitHubAccount!: string;
+	constructor(private dataTransferService: DataTransferService) {}
+
+	sendData() {}
+
 	onSubmit() {
-		this.router.navigate(["ticket"]);
+		const dataToSend = {
+			name: this.name,
+			emailAddress: this.emailAdress,
+			gitHubAccount: this.gitHubAccount,
+		};
+		const dataFromForm = dataToSend;
+		this.dataTransferService.changeData(dataFromForm);
+		console.log(dataFromForm);
+		// this.router.navigate(["ticket"]);
 	}
 }
