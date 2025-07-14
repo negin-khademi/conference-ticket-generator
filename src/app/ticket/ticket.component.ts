fd136f3580b0ae4d5d966f53ac60e753c1832c4a
negin-khademi <negin.khademi@gmail.com>
2025-07-14 21:11:07 +0330

    feat : receive name & email & githubAccount on tichet

--- src/app/ticket/ticket.component.ts
+++ src/app/ticket/ticket.component.ts
@@ -1,7 +1,8 @@
+import { Component, OnInit } from "@angular/core";
 import { DatePipe, TitleCasePipe } from "@angular/common";
 
 import { BackgroundComponent } from "../background/background.component";
-import { Component } from "@angular/core";
+import { DataTransferService } from "./../shared/services/data-transfer.service";
 
 @Component({
 	selector: "app-ticket",
@@ -9,6 +10,15 @@ import { Component } from "@angular/core";
 	templateUrl: "./ticket.component.html",
 	styleUrl: "./ticket.component.scss",
 })
-export class TicketComponent {
+export class TicketComponent implements OnInit {
 	currentDateTime: Date = new Date();
+	receiveData: any;
+
+	constructor(public dataTransferService: DataTransferService) {}
+
+	ngOnInit() {
+		this.dataTransferService.currentdata.subscribe((data) => {
+			this.receiveData = data;
+		});
+	}
 }
